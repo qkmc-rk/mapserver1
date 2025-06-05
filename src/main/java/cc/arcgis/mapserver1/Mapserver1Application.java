79064e829cb095a00a665ed74c7a3eff88ecf017
阮少爷 <314445437@qq.com>
2025-06-05 15:39:56 +0800

    first commit

---
+++ src/main/java/cc/arcgis/mapserver1/Mapserver1Application.java
@@ -0,0 +1,13 @@
+package cc.arcgis.mapserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mapserver1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mapserver1Application.class, args);
+	}
+
+}
---
+++ src/test/java/cc/arcgis/mapserver1/Mapserver1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.arcgis.mapserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mapserver1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
