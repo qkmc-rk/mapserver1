451dc811527ac271f5e4ada8cb28d0f0e1116160
阮少爷 <314445437@qq.com>
2025-06-12 23:42:50 +0800

    初版定稿

--- src/main/java/cc/arcgis/mapserver1/controller/ChatController.java
+++ src/main/java/cc/arcgis/mapserver1/controller/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import reactor.core.publisher.Flux;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class ChatController {
 
     private final OllamaChatModel chatModel;
